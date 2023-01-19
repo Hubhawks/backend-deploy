afbdf723c1a1f261a170f9df7304846d81b5cc23
Hubhawks <119926926+Hubhawks@users.noreply.github.com>
2023-01-19 19:41:52 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const { User } = require("../models/user");
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
 const passport = require("passport");
-const CLIENT_URL = "/googlee";
+const CLIENT_URL = "/";
 router.get("/login/success", (req, res) => {
 	if (req.user) {
 	  res.status(200).json({
